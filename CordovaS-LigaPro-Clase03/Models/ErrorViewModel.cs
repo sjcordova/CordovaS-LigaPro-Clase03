334fcc320b0214a274f6b7187c9c885223c0881d
laboratorio <laboratorio@UPOAULA107>
2025-04-10 07:20:53 -0500

    Agregar archivos de proyecto.

---
+++ CordovaS-LigaPro-Clase03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CordovaS_LigaPro_Clase03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
